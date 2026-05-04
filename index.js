cbbec43f5c498666856f6970771be263e82350d4
Rahul Roy <rahulraghav6385@gmail.com>
2026-05-04 23:59:33 +0530

    removed node_modules and added gitignore

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const fs = require("fs");
 const express = require("express");
-const http = require("http");
 const app = express();
-const PORT = 5500;
+const path = require("path");
+const PORT = 4000;
 
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+let post = [];
 
 app.get('/',(req,res)=>
-    {res.send('backend is running');});
+    {res.render("index");});
 
-app.listen(5500, () => {
-  console.log('Server started on port 8000');
+app.get('/new',(req,res)=>{
+  res.render("new");
+});
+
+app.post("/delete/:id",(req, res) => {
+    let id = req.params.id; // Changed from index to id for clarity
+    post.splice(id, 1);
+    res.redirect("/allblogs");
+});
+
+
+app.get('/allblogs',(req,res)=>{
+  res.render("allblogs",{post : post});
+});
+
+app.post("/submit", (req, res) => {
+
+  let newpost = {
+     title: req.body.title,
+    content: req.body.content ? req.body.content : "No content"
+  };
+
+  post.push(newpost);
+
+  res.redirect("/allblogs");
+});
+
+app.listen(PORT, () => {
+  console.log('Server started on port'+ PORT);
 });
